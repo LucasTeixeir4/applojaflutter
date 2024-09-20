e5e5a09069ac6f0cbda6ae9caefee52b1adc7918
Lucas Teixeira <lucashteixeira9@gmail.com>
2024-09-19 23:25:53 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/apploja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apploja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/usermodel.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String name;
+  final String cpf;
+  final String phone;
+  final String email;
+  final String? id;
+
+  User(
+      {required this.name,
+      required this.cpf,
+      required this.phone,
+      required this.email,
+      this.id});
+
+  //do firestore (json) para dados
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        name: json['name'],
+        cpf: (json['cpf']),
+        phone: json['phone'],
+        email: json['email'],
+        id: json['id']);
+  }
+
+  //dados para firestore (json)
+
+  toJson() {
+    return {
+      'name': name, 
+      'cpf': cpf, 
+      'phone': phone, 
+      'email': email, 
+      'id': id};
+  }
+}
